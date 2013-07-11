1760ef8851c5db3360ad6c8340873ce102a0159e
Charles <charleschoi87@gmail.com>
2013-07-11 16:21:35 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hiyoung::Application.config.secret_token = 'fee31b2ea48f5c72ea5386357a81b422fadfff910ed9737513ca2c3f7447ddfbda8935893688f815d21b34d7b254765101a01db0c6c1f88afea3d1b9bde63317'
