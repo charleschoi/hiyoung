2a6068a41082d48fe3f1584804d39956c220d94c
Charles <charleschoi87@gmail.com>
2013-07-12 15:31:57 +0900

    essay write

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -23,9 +23,19 @@ class StudyController < ApplicationController
   end
 
   def write
+		@topic = Topic.find(params[:id])
   end
 
 	def write_process
+		essay = Essay.new
+		essay.user_id = 1 #current_user.id
+		essay.username = "charles" #current_user.name
+		essay.topic_id = params[:id]
+		essay.content = params[:content]
+		essay.summary = params[:content]
+		essay.save
+
+		redirect_to :controller => "study", :action => "read", :id => essay.id
 	end
 
   def read
